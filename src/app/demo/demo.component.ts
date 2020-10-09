dc7c83bd70af21ce4530716778e2a08919eaf82a
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-10-08 23:22:50 -0500

    Separate all features of the app
    Features was separated to diferent modules, demo with exercises,
    products with the products list.

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -7,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DemoComponent implements OnInit {
 
+  title = 'platzi-store';
+  isPro = true;
+
+  items = ['nicolas', 'julian', 'perez'];
+
+  power = 10;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addItem(): void {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number): void {
+    this.items.splice(index, 1);
   }
 }
