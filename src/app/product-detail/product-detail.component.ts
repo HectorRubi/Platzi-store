17881c5d8b669219a4936ab40050a1c03f2e5c61
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-10-12 13:57:06 -0500

    Get id from route params and get product using it

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
+import { ProductsService } from '../products.service';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      const id = params.id;
+      const product = this.productsService.getProduct(id);
+      console.log(product);
+    });
   }
 
 }
