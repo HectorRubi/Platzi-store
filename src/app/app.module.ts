dcaa051df8eeeda8051c06560d45967899ac3455
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-10-15 18:43:56 -0500

    Remove contact component from app module and import contact module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { CardComponent } from './card/card.component';
 import { ProductsComponent } from './products/products.component';
-import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
@@ -22,7 +21,6 @@ import { LayoutComponent } from './layout/layout.component';
     ProductComponent,
     CardComponent,
     ProductsComponent,
-    ContactComponent,
     DemoComponent,
     PageNotFoundComponent,
     ProductDetailComponent,
