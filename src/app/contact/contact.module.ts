872765d0b4dfcf15fceb999a2459bee40b752a0e
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-10-15 18:43:06 -0500

    Add routing system to contact module

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
@@ -8,7 +9,8 @@ import { ContactComponent } from './components/contact/contact.component';
     ContactComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ContactRoutingModule
   ]
 })
 export class ContactModule { }
