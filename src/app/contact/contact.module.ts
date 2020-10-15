3cbd20a46d039c5e7ed11785d525236bdece4181
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-10-15 18:42:36 -0500

    Import contact component at contact module

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContactComponent
+  ],
   imports: [
     CommonModule
   ]
