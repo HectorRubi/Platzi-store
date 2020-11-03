f753b46e7dbc9c79965e1b2f095b90056fdf5395
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-11-02 18:55:32 -0600

    Create login and register forms

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  hide = true;
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.buildForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  get emailField(): AbstractControl {
+    return this.form.get('email');
+  }
+
+  get passwordField(): AbstractControl {
+    return this.form.get('password');
+  }
+
+  buildForm(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  handlePassword(event: Event): void {
+    event.preventDefault();
+    this.hide = !this.hide;
+  }
+
+  login(event: Event): void {
+    event.preventDefault();
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
   }
 
 }
