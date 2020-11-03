a34d80843fb8ac63820fa3f5a4e6446341ff5674
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-11-03 12:01:58 -0600

    Add environment variable firebaseConfig with firebase keys

--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  url_api: 'http://platzi-store-stag.herokuapp.com'
+  url_api: 'http://platzi-store-stag.herokuapp.com',
+  firebaseConfig: {
+    apiKey: 'AIzaSyArp1YAuc_HV6ox5H3d2qu9usDhpgsIhfs',
+    authDomain: 'platzi-store-fc265.firebaseapp.com',
+    databaseURL: 'https://platzi-store-fc265.firebaseio.com',
+    projectId: 'platzi-store-fc265',
+    storageBucket: 'platzi-store-fc265.appspot.com',
+    messagingSenderId: '850830306077',
+    appId: '1:850830306077:web:843b939a4a86393a03cff4'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-  url_api: 'http://platzi-store.herokuapp.com'
+  url_api: 'http://platzi-store.herokuapp.com',
+  firebaseConfig: {
+    apiKey: 'AIzaSyArp1YAuc_HV6ox5H3d2qu9usDhpgsIhfs',
+    authDomain: 'platzi-store-fc265.firebaseapp.com',
+    databaseURL: 'https://platzi-store-fc265.firebaseio.com',
+    projectId: 'platzi-store-fc265',
+    storageBucket: 'platzi-store-fc265.appspot.com',
+    messagingSenderId: '850830306077',
+    appId: '1:850830306077:web:843b939a4a86393a03cff4'
+  }
 };
 
 /*
